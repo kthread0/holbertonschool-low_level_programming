9488e8d3a4e2c249fba531635f756d1d742e4b66
kthread0 <dev@fault.wtf>
2025-06-23 11:38:31 -0300

    pos

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *	main - entry point
+ *	Return: number
+ */
+
+int main(void) 
+{
+  int n = 0;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+  if (n > 0) {
+    printf("%d is positive\n", n);
+  } else {
+    printf("%d is negative\n", n);
+  }
+
+  return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,2 @@
+# Variables
+Folder dedicated to variables, and loops
