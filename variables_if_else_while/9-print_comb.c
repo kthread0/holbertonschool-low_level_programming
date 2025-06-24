fa64e60e2b30ab47c21fd4843246a0ece8e54950
kthread0 <dev@fault.wtf>
2025-06-24 12:18:14 -0300

    yay

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *	main - entry point
+ *	Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57) {
+			putchar(44);
+			putchar(32);
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
