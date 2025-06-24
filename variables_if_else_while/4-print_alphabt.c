7f1bb8b1756ea78ff9eb3dc674d9f8e9ec058695
kthread0 <dev@fault.wtf>
2025-06-24 11:56:32 -0300

    print alphabet

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ *	main - entry point
+ *	Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if(i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
