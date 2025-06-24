c84e42da2fff88f26412b1b44dff9cdf5f4d8fb0
kthread0 <dev@fault.wtf>
2025-06-24 11:35:46 -0300

    print entire alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *	main - entry point
+ *	Return: 0
+ */
+
+int main(void)
+{
+	int i = 0;
+	for (i = 97; i < 123; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
