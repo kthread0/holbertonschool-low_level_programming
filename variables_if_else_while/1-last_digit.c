b8c116f1caad4b72be8ece0de59ea9d9c2f11e74
kthread0 <dev@fault.wtf>
2025-06-24 11:14:18 -0300

    fix

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -7,22 +7,26 @@
  *	Return: 0
  */
 
-int main(void) {
-  int n;
-  int last;
+int main(void)
+{
+	int n;
+	int last;
 
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-  last = n % 10;
+	last = n % 10;
 
-  if (last > 5) {
-    printf("Last digit of %d is %d and is greater than 5\n", n, last);
-  } else if (last == 0) {
-    printf("Last digit of %d is 0 and is 0\n", n);
-  } else {
-    printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-  }
+	if (last > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, last);
+	} else if (last == 0)
+	{
+		printf("Last digit of %d is 0 and is 0\n", n);
+	} else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+	}
 
-  return (0);
+	return (0);
 }
