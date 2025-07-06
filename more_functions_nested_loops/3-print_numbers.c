2e57f2fc99b3981ff3782d79618065f621e48b79
kthread0 <dev@fault.wtf>
2025-07-06 12:58:33 -0300

    print numbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9, followed by a new line
+ *
+ * Description: This function prints digits 0 through 9 in sequence,
+ * then prints a newline character
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		_putchar(digit + '0');
+	}
+
+	_putchar('\n');
+}
