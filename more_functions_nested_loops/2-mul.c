ecf98abf1df244b2a2b33c9cf90d37e538b30648
kthread0 <dev@fault.wtf>
2025-07-06 12:56:20 -0300

    multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - entry point for function
+ * @a: first integer to multiply
+ * @b: second integer to multiply
+ *
+ * Return: the result of a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
